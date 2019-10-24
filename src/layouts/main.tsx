5c6d25ef77da9a390ee8ed26e84123dc63f4f8c8
Fabrice Million <fabrice@funkadelik.com>
2019-10-24 17:04:39 +1100

    added different components to layout

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,17 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import ClientHeader from '../components/ClientHeader';
 import { useRouter } from 'next/router';
-// import { Location } from "@reach/router"
 import Header from '../components/header';
-// import SearchApp from './search/_Search';
+import SearchApp from '../components/search/_Search';
 import MainNavigation from '../components/MainNavigation';
 import styled from 'styled-components';
 import { ContentRow } from '../components/grid';
@@ -22,30 +14,47 @@ import SiblingsMenu from '../components/SiblingsMenu';
 
 const IsLite = nodes => some(nodes, 'Disabled');
 
-const Layout = ({ children, alias, navnodes, products }) => {
+const Layout = ({ children, client, navnodes, products, sitemapGroup }) => {
   return (
     <>
       {useRouter().pathname === '/' ? (
         <Header siteTitle={'Find your economic profile…'} />
       ) : (
-        <React.Fragment>
+        <>
+          <SearchApp
+            alias={client.Alias}
+            clientID={client.clientID}
+            longName={client.LongName}
+            clientImage={'data.clientLogo.imageData.fixed'}
+          />
           <ClientHeader
-            alias={'MONASH'}
-            name={'monash'}
-            longName={'monash'}
+            alias={client.Alias}
+            name={client.Name}
+            longName={client.LongName}
             products={products}
-            clientID={102}
+            clientID={client.clientID}
             clientImage={'data.clientLogo.imageData.fixed'}
             isLite={IsLite(navnodes)}
           />
-        </React.Fragment>
+        </>
       )}
       <ContentRow>
         <SidebarNav>
-          <MainNavigation alias={alias} navigationNodes={navnodes} />
+          <MainNavigation alias={client.alias} navigationNodes={navnodes} />
         </SidebarNav>
-        <SiteContent>{children}</SiteContent>
+        <SiteContent>
+          <SiblingsMenu navigationNodes={navnodes} clientAlias={client.alias} />
+          {children}
+        </SiteContent>
       </ContentRow>
+      <SiteMap
+        alias={client.Alias}
+        colGroups={sitemapGroup}
+        longName={client.LongName}
+        products={products}
+        navigationNodes={navnodes}
+      />
+      <SharedFooter />
     </>
   );
 };
