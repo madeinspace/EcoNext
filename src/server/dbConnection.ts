29a551e022dcfa59f5bab950f79dd4515c4966e5
Fabrice Million <fabrice@funkadelik.com>
2019-11-15 17:09:46 +1100

    refactored dbConnection

--- src/server/dbConnection.ts
+++ src/server/dbConnection.ts
@@ -11,31 +11,25 @@ const getScopedEnvVar = (scope, db_env_var) => {
   );
 };
 
-const CommClientDBConnection = () => ({
-  client: 'mssql',
-  connection: {
-    host: getScopedEnvVar('CLIENT', 'DATABASE_HOST'),
-    user: getScopedEnvVar('CLIENT', 'DATABASE_USER'),
-    password: getScopedEnvVar('CLIENT', 'DATABASE_PASS'),
-    database: getScopedEnvVar('CLIENT', 'DATABASE_NAME'),
-    requestTimeout: 0,
-    options: { encrypt: false }
-  },
-  acquireConnectionTimeout: 600000
-});
+const client = 'mssql';
+const connection = {
+  host: getScopedEnvVar('CLIENT', 'DATABASE_HOST'),
+  user: getScopedEnvVar('CLIENT', 'DATABASE_USER'),
+  password: getScopedEnvVar('CLIENT', 'DATABASE_PASS'),
+  database: getScopedEnvVar('CLIENT', 'DATABASE_NAME'),
+  requestTimeout: 0,
+  options: { encrypt: false }
+};
+const acquireConnectionTimeout = 600000;
+const DBConnection = {
+  client,
+  connection,
+  acquireConnectionTimeout
+};
 
-const CommDataEconomyDBConnection = () => ({
-  client: 'mssql',
-  connection: {
-    host: getScopedEnvVar('DATA', 'DATABASE_HOST'),
-    user: getScopedEnvVar('DATA', 'DATABASE_USER'),
-    password: getScopedEnvVar('DATA', 'DATABASE_PASS'),
-    database: getScopedEnvVar('DATA', 'DATABASE_NAME'),
-    requestTimeout: 0,
-    options: { encrypt: false }
-  },
-  acquireConnectionTimeout: 600000
-});
+const CommClientDBConnection = () => DBConnection;
+
+const CommDataEconomyDBConnection = () => DBConnection;
 
 export const commDataEconomy = knex(CommDataEconomyDBConnection());
 export const commClient = knex(CommClientDBConnection());
