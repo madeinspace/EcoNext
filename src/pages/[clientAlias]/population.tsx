f29a7eebfabeecbe203c66649888d1297d923d39
Fabrice Million <fabrice@funkadelik.com>
2019-11-15 17:09:59 +1100

    WIP: added population page

--- src/pages/[clientAlias]/population.tsx
+++ src/pages/[clientAlias]/population.tsx
@@ -1,22 +1,135 @@
+// #region imports
 import fetch from 'isomorphic-unfetch';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
+import _ from 'lodash';
 import Layout from '../../layouts/main';
-import { getPort } from '../../Utils';
+import {
+  formatShortDecimal,
+  formatNumber,
+  formatChangeNumber,
+  formatChangePercent,
+  getPort
+} from '../../Utils';
+import {
+  TitleContainer,
+  MainTitle,
+  SubTitle,
+  Headline,
+  PageIntro,
+  ItemWrapper,
+  CrossLink,
+  EntityContainer,
+  ForecastProductIcon
+} from '../../styles/MainContentStyles';
+import { Actions, Share, ExportPage } from '../../components/Actions';
+import EntityTable from '../../components/table/EntityTable';
+import EntityChart from '../../components/chart/EntityChart';
+import qs from 'qs';
+// #endregion
 
-const Population = ({ client, navigation, clientProducts, sitemapGroups }) => {
+// #region population page
+const Population = ({
+  client,
+  tableData,
+  navigation,
+  clientProducts,
+  sitemapGroups
+}) => {
+  const { LongName: prettyName } = client;
+  const router = useRouter();
+  const hasForecast = clientProducts =>
+    _.some(clientProducts, product => product.ApplicationID === 3);
+
+  const FormattedNumber = number => <>{formatNumber(number)}</>;
+  const handleExport = () => {};
+
+  const setQuery = (key, value) => {
+    const query = { ...qs.parse(location.search, { ignoreQueryPrefix: true }) };
+    query[key] = value;
+    Router.push({
+      pathname: `/${clientAlias}/population`,
+      query: { ...query }
+    });
+  };
   const { clientAlias } = useRouter().query;
+  const chartData = chartBuilder(tableData);
+  const chartLineData = chartLineBuilder(tableData);
+  const tableParams = tableBuilder(clientAlias, tableData);
+
   return (
     <Layout
       client={client}
       navnodes={navigation}
       products={clientProducts}
       sitemapGroup={sitemapGroups}
-    ></Layout>
+    >
+      <EntityContainer>
+        <TitleContainer>
+          <MainTitle>{prettyName}</MainTitle>
+          <SubTitle>Population</SubTitle>
+        </TitleContainer>
+        <Actions>
+          <Share />
+          <ExportPage
+            onExport={e => handleExport()}
+            exportOptions={{
+              formats: [{ displayText: 'Word' } /*, { name: "PDF" }*/]
+            }}
+          />
+        </Actions>
+      </EntityContainer>
+      <Headline>
+        The Estimated Resident Population of the {prettyName} was{' '}
+        <FormattedNumber number={12} /> as of the 30th June [latestYear].
+      </Headline>
+      <PageIntro>
+        <p>
+          The Estimated Resident Population (ERP) is the official population of
+          the area. It is updated annually by the Australian Bureau of
+          Statistics, and reassessed every Census. The chart and table show last
+          10 years ERP for {prettyName}, the state and Australia, with
+          percentage comparisons. A growing population can indicate a growing
+          economy, but this is not necessarily the case and depends on the
+          residential role and function of the area.
+        </p>
+      </PageIntro>
+
+      <ItemWrapper>
+        <EntityChart data={chartData} />
+      </ItemWrapper>
+
+      <ItemWrapper>
+        <EntityChart data={chartLineData} />
+      </ItemWrapper>
+
+      <ItemWrapper>
+        <EntityTable
+          data={tableParams}
+          name={'Local workers - field of qualification'}
+        />
+      </ItemWrapper>
+
+      {hasForecast(clientProducts) && (
+        <CrossLink>
+          <ForecastProductIcon />
+          <a
+            href={`http://forecast.id.com.au/${clientAlias}/population-summary?WebId=10`}
+            target="_blank"
+            title="link to forecast"
+          >
+            Population forecasts
+            <span className="hidden"> (opens a new window)</span>
+          </a>
+        </CrossLink>
+      )}
+    </Layout>
   );
 };
 
 export default Population;
+// #endregion
 
+// #region getInitialProps
 Population.getInitialProps = async function({ query }) {
   const { clientAlias } = query;
   const data = await fetch(
@@ -24,3 +137,358 @@ Population.getInitialProps = async function({ query }) {
   ).then(res => res.json());
   return data;
 };
+// #endregion
+
+// #region Source
+const Source = () => (
+  <>
+    Source: Australian Bureau of Statistics, Regional Population Growth,
+    Australia (3218.0). Compiled and presented in economy.id by{' '}
+    <a
+      href="http://home.id.com.au/about-us/"
+      target="_blank"
+      title=".id website"
+    >
+      .id, the population experts.
+      <span className="hidden"> (opens a new window)</span>
+    </a>
+  </>
+);
+// #endregion
+
+// #region tableBuilder
+const tableBuilder = (alias, nodes) => {
+  return {
+    cssClass: '',
+    allowExport: false,
+    allowSort: true,
+    allowSortReset: true,
+    groupOn: '',
+    clientAlias: alias,
+    source: <Source />,
+    anchorName: 'service-age-groups',
+    headRows: [
+      {
+        cssClass: '',
+        cols: [
+          {
+            cssClass: 'table-area-name',
+            displayText: 'Annual change in Estimated Resident Population (ERP)',
+            colSpan: 10,
+            rowSpan: 0
+          }
+        ],
+        key: 'hr0'
+      },
+      {
+        cssClass: '',
+        cols: [
+          {
+            cssClass: '',
+            displayText: '',
+            colSpan: 1,
+            rowSpan: 0
+          },
+          {
+            cssClass: 'xeven start-year',
+            displayText: nodes[0].ClientGeoName,
+            colSpan: 3,
+            rowSpan: 0
+          },
+          {
+            cssClass: 'xodd end-year',
+            displayText: nodes[0].BenchmarkGeoName,
+            colSpan: 3,
+            rowSpan: 0
+          },
+          {
+            cssClass: 'xeven start-year',
+            displayText: nodes[0].AusGeoName,
+            colSpan: 3,
+            rowSpan: 0
+          }
+        ],
+        key: 'hr1'
+      }
+    ],
+    cols: [
+      {
+        id: 0,
+        displayText: 'Year (ending June 30)',
+        dataType: 'int',
+        sortable: true,
+        cssClass: 'xodd xfirst'
+      },
+      {
+        id: 1,
+        displayText: 'Number',
+        dataType: 'int',
+        sortable: true,
+        cssClass: 'xeven latest',
+        format: '{0:#,0}'
+      },
+      {
+        id: 2,
+        displayText: 'Change in number',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'xeven latest',
+        format: '{0:+#,0;-#,0;0}'
+      },
+      {
+        id: 3,
+        displayText: 'Change in percent',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'xeven latest',
+        format: '{0:+#,0;-#,0;0}'
+      },
+      {
+        id: 4,
+        displayText: 'Number',
+        title: '',
+        dataType: 'int',
+        sortable: true,
+        cssClass: 'xodd',
+        format: '{0:#,0}'
+      },
+      {
+        id: 5,
+        displayText: 'Change in number',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'per xodd',
+        format: '{0:+#,0;-#,0;0}'
+      },
+      {
+        id: 6,
+        displayText: 'Change in percent',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'xodd',
+        format: '{0:+#,0;-#,0;0}'
+      },
+      {
+        id: 7,
+        displayText: 'Number',
+        title: '',
+        dataType: 'int',
+        sortable: true,
+        cssClass: 'xeven',
+        format: '{0:#,0}'
+      },
+      {
+        id: 8,
+        displayText: 'Change in number',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'per xeven',
+        format: '{0:+#,0;-#,0;0}'
+      },
+      {
+        id: 9,
+        displayText: 'Change in percent',
+        dataType: 'money',
+        sortable: true,
+        cssClass: 'xeven',
+        format: '{0:+#,0;-#,0;0}'
+      }
+    ],
+    footRows: [],
+    rows: nodes.map(
+      (
+        {
+          ClientYear,
+          ClientNumber,
+          ClientChangeYear1Year2,
+          ClientChangeper,
+          BenchmarkNumber,
+          BenchmarkChangeYear1Year2,
+          BenchmarkChangeper,
+          AusNumber,
+          AusChangeYear1Year2,
+          AusChangeper
+        },
+        i: number
+      ) => ({
+        data: [
+          ClientYear,
+          ClientNumber,
+          ClientChangeYear1Year2,
+          ClientChangeper,
+          BenchmarkNumber,
+          BenchmarkChangeYear1Year2,
+          BenchmarkChangeper,
+          AusNumber,
+          AusChangeYear1Year2,
+          AusChangeper
+        ],
+        formattedData: [
+          ClientYear,
+          formatNumber(ClientNumber),
+          formatChangeNumber(ClientChangeYear1Year2, '--'),
+          formatChangePercent(ClientChangeper, '--'),
+          formatNumber(BenchmarkNumber),
+          formatChangeNumber(BenchmarkChangeYear1Year2, '--'),
+          formatChangePercent(BenchmarkChangeper, '--'),
+          formatNumber(AusNumber),
+          formatChangeNumber(AusChangeYear1Year2, '--'),
+          formatChangePercent(AusChangeper, '--')
+        ],
+        id: i
+      })
+    ),
+    noOfRowsOnInit: 11
+  };
+};
+// #endregion
+
+// #region chartLineBuilder
+const chartLineBuilder = nodes => {
+  return {
+    cssClass: '',
+    highchartOptions: {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Estimated Resident Population (ERP)',
+        align: 'left'
+      },
+      tooltip: {
+        pointFormatter: function() {
+          return `<span class="highcharts-color-${
+            this.colorIndex
+          }">\u25CF</span> ${this.series.name}: ${formatShortDecimal(this.y)}%`;
+        }
+      },
+      series: [
+        {
+          color: '',
+          yAxis: 0,
+          name: nodes[0].ClientGeoName,
+          data: _.map(nodes, 'ClientChangeper').reverse()
+        },
+        {
+          color: '',
+          yAxis: 0,
+          name: nodes[0].BenchmarkGeoName,
+          data: _.map(nodes, 'BenchmarkChangeper').reverse()
+        },
+        {
+          color: '',
+          yAxis: 0,
+          name: nodes[0].AusGeoName,
+          data: _.map(nodes, 'AusChangeper').reverse()
+        }
+      ],
+      xAxis: {
+        categories: _.map(nodes, 'ClientYear').reverse(),
+        croshair: false,
+        title: {
+          text: 'Year ending June',
+          align: 'low'
+        },
+
+        labels: {
+          staggerLines: 0,
+          format: ''
+        },
+        opposite: false,
+        plotBands: []
+      },
+      yAxis: [
+        {
+          croshair: false,
+          title: {
+            text: 'Percentage change'
+          },
+          labels: {
+            staggerLines: 0,
+            formatter: function() {
+              return formatChangeNumber(this.value);
+            }
+          },
+          opposite: false,
+          plotBands: []
+        }
+      ]
+    },
+    rawDataSource:
+      'Source: Australian Bureau of Statistics, Regional Population Growth, Australia (3218.0). Compiled and presented in economy.id by .id, the population experts.',
+    dataSource: <Source />,
+    chartContainerID: 'chart2',
+    logoUrl: 'http://profile.local.com.au:8666/dist/images/id-logo.png',
+    entityID: 1,
+    chartTemplate: 'Standard'
+  };
+};
+// #endregion
+
+// #region  chartbuilder
+const chartBuilder = nodes => {
+  return {
+    cssClass: '',
+    highchartOptions: {
+      chart: {
+        type: 'column',
+        styledMode: true
+      },
+      title: {
+        text: 'Estimated Resident Population (ERP)',
+        align: 'left'
+      },
+      subtitle: {
+        text: nodes[0].ClientGeoName,
+        align: 'left'
+      },
+      series: [
+        {
+          color: '',
+          yAxis: 0,
+          name: nodes[0].ClientGeoName,
+          data: _.map(nodes, 'ClientNumber').reverse()
+        }
+      ],
+      xAxis: {
+        categories: _.map(nodes, 'ClientYear').reverse(),
+        croshair: false,
+        title: {
+          text: 'Year ending June',
+          align: 'low'
+        },
+        labels: {
+          staggerLines: 0,
+          format: ''
+        },
+        opposite: false,
+        plotBands: []
+      },
+      yAxis: [
+        {
+          croshair: false,
+          title: {
+            text: 'Total Estimated Resident Population (ERP)',
+            align: 'low'
+          },
+          labels: {
+            staggerLines: 0,
+            formatter: function() {
+              return formatNumber(this.value);
+            }
+          },
+          opposite: false,
+          plotBands: []
+        }
+      ]
+    },
+    rawDataSource:
+      'Source: Australian Bureau of Statistics, Regional Population Growth, Australia (3218.0). Compiled and presented in economy.id by .id, the population experts.',
+    dataSource: <Source />,
+    chartContainerID: 'chart1',
+    logoUrl: '/images/id-logo.png',
+    entityID: 1,
+    chartTemplate: 'Standard'
+  };
+};
+// #endregion
--- src/pages/api/[clientAlias]/population.ts
+++ src/pages/api/[clientAlias]/population.ts
@@ -3,12 +3,16 @@ import { commClient, commDataEconomy } from '../../../server/dbConnection';
 
 const handle = async (req, res) => {
   const { clientAlias } = req.query;
-  const clients = await commClient.raw(ClientSQL);
-  const client: any = clientFromAlias(clientAlias, clients);
-  const navigation = await commClient.raw(MainNavigationSQL({ client }));
-  const clientProducts = await commClient.raw(ClientProductsSQL({ client }));
+  const client = await commClient
+    .raw(ClientSQL({ clientAlias }))
+    .then(res => res[0]);
+  const ClientID = client.ClientID;
+  const navigation = await commClient.raw(MainNavigationSQL({ ClientID }));
+  const clientProducts = await commClient.raw(ClientProductsSQL({ ClientID }));
+  const sitemapGroups = await commDataEconomy.raw(SitemapGroupsSQL());
+  const tableData = await commDataEconomy.raw(PopulationDataSQL({ ClientID }));
 
-  res.json({ client, navigation, clientProducts });
+  res.json({ client, tableData, navigation, clientProducts, sitemapGroups });
 };
 export default handle;
 
@@ -22,61 +26,44 @@ const clientFromAlias = (clientAlias, clients) =>
 const getScopedEnvVar = (scope, db_env_var) =>
   process.env[`${scope}_${db_env_var}`] || process.env[`DEFAULT_${db_env_var}`];
 
-const ClientSQL = `
-  WITH RDAS AS (
-    SELECT
-      DISTINCT(areas.ClientID),
-      CASE WHEN RDAs.IsRDA IS NOT NULL THEN 1 ELSE 0 END AS IsRDA
-      FROM [CommClient].[dbo].[ClientToAreas_Economy] areas
-      LEFT OUTER JOIN (
-      SELECT
-        DISTINCT(ClientID),
-        COUNT(WebID) AS IsRDA
-      FROM [CommClient].[dbo].[ClientToAreas_Economy] areas
-        WHERE WebID > 50
-        GROUP BY (ClientID)
-      ) AS RDAs
-      ON areas.ClientID = RDAs.ClientID
-    )
-    SELECT
-      client.ClientID,
-      client.Name,
-      client.ShortName,
-      client.LongName,
-      client.Alias
-    FROM Client AS client
-    INNER JOIN ClientAppDisable AS clientMeta
-      ON clientMeta.ClientID = client.ClientID
-    INNER JOIN RDAS
-      ON client.ClientID = RDAS.ClientID
-    WHERE clientMeta.IsDisabled = 0
-
-    
-      AND clientMeta.ApplicationID = 4
-      AND RDAS.IsRDA = 0
-    ${ignoreClients
-      .map(clientAlias => `  AND NOT client.Alias = '${clientAlias}'`)
-      .join('\n')}
+/* #region ClientSQL*/
+const ClientSQL = ({ clientAlias }) => `
+  SELECT
+    client.ClientID,
+    client.Name,
+    client.ShortName,
+    client.LongName,
+    client.Alias
+  FROM Client AS client
+  INNER JOIN CommClient.dbo.ClientAppDisable AS clientMeta
+    ON clientMeta.ClientID = client.ClientID
+  WHERE clientMeta.IsDisabled = 0
+    AND clientMeta.ApplicationID = 4
+    AND Alias = '${clientAlias}'
 `;
+/* #endregion */
 
-const MainNavigationSQL = ({ client }) => `
-  SELECT [ClientID]
-    ,cp.[PageID]
-    ,COALESCE(cp.[Alias], p.Alias) AS Alias
-    ,p.ParentPageID
-    ,p.MenuTitle
-    ,[Disabled]
-    ,pg.Name AS GroupName
-  FROM [CommClient].[dbo].[ClientPage] cp
-  RIGHT JOIN [CommApp].dbo.Page p 
-    ON cp.pageId = p.pageID 
-  INNER JOIN [CommApp].[dbo].[PageGroup] pg
-    ON p.PageGroupID = pg.PageGroupID
-    AND p.ApplicationID = 4
-  WHERE ClientID = ${client.ClientID}
+/* #region  MainNavigationSQL */
+const MainNavigationSQL = ({ ClientID }) => `
+SELECT [ClientID]
+  ,cp.[PageID]
+  ,COALESCE(cp.[Alias], p.Alias) AS Alias
+  ,p.ParentPageID
+  ,p.MenuTitle
+  ,[Disabled]
+  ,pg.Name AS GroupName
+FROM [CommClient].[dbo].[ClientPage] cp
+RIGHT JOIN [CommApp].dbo.Page p 
+  ON cp.pageId = p.pageID 
+INNER JOIN [CommApp].[dbo].[PageGroup] pg
+  ON p.PageGroupID = pg.PageGroupID
+  AND p.ApplicationID = 4
+WHERE ClientID = ${ClientID}
 `;
+/* #endregion */
 
-const ClientProductsSQL = ({ client }) => `
+/* #region  ClientProductsSQL */
+const ClientProductsSQL = ({ ClientID }) => `
   SELECT 
      c.Alias AS Alias
     ,c.name AS ClientLongName
@@ -91,5 +78,113 @@ const ClientProductsSQL = ({ client }) => `
   LEFT OUTER JOIN [CommClient].[dbo].[Client] AS c
     ON cad.ClientID = c.ClientID
   WHERE cad.IsDisabled = 0
-    AND cad.ClientID = ${client.ClientID}
+    AND cad.ClientID = ${ClientID}
 `;
+/* #endregion */
+
+/* #region  SitemapGroupsSQL */
+const SitemapGroupsSQL = () => `
+  SELECT TOP (1000) [ApplicationID]
+    ,[GroupName]
+    ,[SitemapName]
+    ,[ColNumber]
+    ,[SortOrder]
+    ,[Pages]
+  FROM [CommApp].[dbo].[SitemapInfo]
+  where SitemapName like 'footer'
+  and ApplicationID = 4
+`;
+/* #endregion */
+
+const PopulationDataSQL = ({ ClientID }) => `
+  With CTE AS (
+    select 
+      [ClientID]
+      ,[WebID]
+      ,[GeoName]
+      ,[Year]
+      ,[Number]
+      ,CASE WHEN [WebID] = 10 THEN 1 ELSE 2 END as [GeoType]
+    from CommData_Economy.[dbo].[v_IN_ERPPivot]
+    where WebID IN (10, 40, 50)
+      AND ClientID = ${ClientID}
+  ),
+  CTE2 AS (
+    select [ClientID]
+      ,[WebID]
+      ,[GeoName]
+      ,[Year] + 1 as [Year]
+      ,[Number]
+      ,[GeoType]
+    from CTE
+  )
+  
+  select C.ClientID as ClientID
+    ,C.WebID as ClientWebID
+    ,C.Geoname as ClientGeoName
+    ,C.Year as ClientYear
+    ,C.Number as ClientNumber
+    ,C.ChangeYear1Year2 as ClientChangeYear1Year2
+    ,C.Changeper as ClientChangeper
+    
+    ,BM.WebID as BenchmarkWebID
+    ,BM.Geoname as BenchmarkGeoName
+    ,BM.Year as BenchmarkYear
+    ,BM.Number as BenchmarkNumber
+    ,BM.ChangeYear1Year2 as BenchmarkChangeYear1Year2
+    ,BM.Changeper as BenchmarkChangeper
+    
+    ,AUS.WebID as AusWebID
+    ,AUS.Geoname as AusGeoName
+    ,AUS.Year as AusYear
+    ,AUS.Number as AusNumber
+    ,AUS.ChangeYear1Year2 as AusChangeYear1Year2
+    ,AUS.Changeper as AusChangeper
+  from (
+    SELECT CTE_1.ClientID, 
+      CTE_1.WebID,
+      CTE_1.GeoName, 
+      CTE_1.Year, 
+      CTE_1.Number,
+      CTE_1.Number  - CTE4.Number as  ChangeYear1Year2,
+      (CAST((CTE_1.Number - CTE4.Number) as float)/CTE4.Number)*100 as ChangePer
+    from CTE as CTE_1 
+    Left Hash Join CTE2 as CTE4
+            on  CTE_1.ClientId = CTE4.ClientId
+            and CTE_1.Webid = CTE4.Webid
+            and CTE_1.Year = CTE4.Year
+    where CTE_1.webid=10
+  ) as C
+    
+  inner join (
+    SELECT CTE_1.ClientID, 
+      CTE_1.WebID,
+      CTE_1.GeoName,
+      CTE_1.Year, 
+      CTE_1.Number,
+      CTE_1.Number  - CTE4.Number as  ChangeYear1Year2,
+      (CAST((CTE_1.Number - CTE4.Number) as float)/CTE4.Number)*100 as ChangePer
+    from CTE as CTE_1 
+    Left Hash Join CTE2 as CTE4 on CTE_1.ClientId = CTE4.ClientId and  CTE_1.Webid = CTE4.Webid and  CTE_1.Year = CTE4.Year
+    where CTE_1.WebID=40
+  ) as BM
+    on  C.ClientID=BM.ClientID
+    AND C.Year=BM.Year
+  
+  inner join (
+    SELECT CTE_1.ClientID, 
+      CTE_1.WebID,
+      CTE_1.GeoName,
+      CTE_1.Year, 
+      CTE_1.Number,
+      CTE_1.Number  - CTE4.Number as  ChangeYear1Year2,
+      (CAST((CTE_1.Number - CTE4.Number) as float)/CTE4.Number)*100 as ChangePer
+    from CTE as CTE_1 
+    Left Hash Join CTE2 as CTE4 on CTE_1.ClientId = CTE4.ClientId and  CTE_1.Webid = CTE4.Webid and  CTE_1.Year = CTE4.Year
+    where CTE_1.WebID=50
+  ) as AUS
+    on  C.Year=AUS.Year
+    AND C.ClientID=AUS.ClientID
+
+    ORDER BY ClientID, ClientYear ASC
+            `;
