56fc2254ea063545abb4e75f53f2a15782494390
Fabrice Million <fabrice@funkadelik.com>
2019-11-21 16:05:58 +1100

    renamed filteredPages to normalisedPages

--- src/utils/fetchNavigation/index.ts
+++ src/utils/fetchNavigation/index.ts
@@ -1,17 +1,14 @@
 const fetchNavigation = async ({ containers, Pages }) => {
   const { PageContainer } = containers;
-
   const { resources: pages } = await PageContainer.items.query(`SELECT * FROM c WHERE c.ApplicationID = 4`).fetchAll();
-
-  const filteredPages = pages.reduce((acc, cur) => ({ ...acc, [cur.PageID]: cur }), {});
-
+  const normalisedPages = pages.reduce((acc, cur) => ({ ...acc, [cur.PageID]: cur }), {});
   const navigation = Pages.filter(({ AppID }) => AppID === 4);
 
   return navigation.map(nav => ({
     ...nav,
-    GroupName: filteredPages[nav.PageID]['GroupDetails']['Name'],
-    MenuTitle: filteredPages[nav.PageID]['MenuTitle'],
-    ParentPageID: filteredPages[nav.PageID]['ParentPageID'],
+    GroupName: normalisedPages[nav.PageID]['GroupDetails']['Name'],
+    MenuTitle: normalisedPages[nav.PageID]['MenuTitle'],
+    ParentPageID: normalisedPages[nav.PageID]['ParentPageID'],
   }));
 };
 
