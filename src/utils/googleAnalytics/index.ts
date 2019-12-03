3c97b80476f2e4f7f1cd348f7bd961e059d86ff0
Fabrice Million <fabrice@funkadelik.com>
2019-12-04 10:53:54 +1100

    Fix: cater for client landing pages

--- src/utils/googleAnalytics/index.ts
+++ src/utils/googleAnalytics/index.ts
@@ -2,11 +2,12 @@
 
 import ReactGA from 'react-ga';
 
-export const initGA = () => {
-  ReactGA.initialize('UA-27808229-5');
+export const initGA = async () => {
+  return ReactGA.initialize('UA-27808229-5');
 };
 
-export const logPageView = ({ clientID, page }) => {
-  ReactGA.set({ page, clientID });
-  ReactGA.pageview(window.location.pathname);
+export const logPageView = ({ clientAlias, page }) => {
+  console.log('clientAlias, page: ', clientAlias, page);
+  ReactGA.set({ page, clientAlias });
+  ReactGA.pageview(page);
 };
