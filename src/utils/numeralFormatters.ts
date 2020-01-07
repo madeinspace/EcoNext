7bbb89d5adc5c5fb9e06cca0c948e26b8ae2ee7f
Fabrice Million <fabrice@funkadelik.com>
2020-01-07 19:17:06 +1100

    Fix: formatNumber displays '--' if number is undefined

--- src/utils/numeralFormatters.ts
+++ src/utils/numeralFormatters.ts
@@ -1,11 +1,11 @@
 import numeral from 'numeral';
 
-export const formatNumber = number => numeral(number).format('0,0');
+export const formatNumber = number => (number != null ? numeral(number).format('0,0') : '--'); // ie: 3,326,812
 export const formatPercent = (number, zero = '0') => (number ? numeral(number).format('0,0.0') : zero);
-export const formatChangeNumber = (number, zero = '0') => (number ? numeral(number).format('+0,0.0') : zero);
+export const formatChangeNumber = (number, zero = '0') => (number ? numeral(number).format('+0,0.00') : zero); //ie: +3.21
 export const formatChangeInt = (number, zero = '0') => (number ? numeral(number).format('+0,0') : zero);
 export const formatChangePercent = (number, zero = '0') => (number ? numeral(number).format('+0,0.00') : zero);
-export const formatShortDecimal = (number, zero = '0') => (number ? numeral(number).format('0.0') : zero);
+export const formatShortDecimal = (number, zero = '0') => (number ? numeral(number).format('0.00') : zero); // ie: 4.37
 export const formatMillionsCurrency = (number, zero = '0') => (number ? numeral(number).format('$0a') : zero);
 export const formatMillionsCurrencyNoRounding = (number, zero = '0') =>
   number ? numeral(number).format('$0.000a') : zero;
