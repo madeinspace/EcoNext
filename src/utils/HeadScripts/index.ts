d94424469582dbabb0a4f4d06bf501208e16072c
Fabrice Million <fabrice@funkadelik.com>
2020-01-09 19:54:53 +1100

    WIP:Experiment removing GA and addthis to fix FOUC on firefox

--- src/utils/HeadScripts/index.ts
+++ src/utils/HeadScripts/index.ts
@@ -1,6 +1,6 @@
 export default [
-  {
-    src: 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-581aca8105d1673e',
-    async: true,
-  },
+  // {
+  //   src: 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-581aca8105d1673e',
+  //   async: true,
+  // },
 ];
