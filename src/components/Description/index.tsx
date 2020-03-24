e391328c9038d5d7455ef4ef0f7a7b7b94a21052
Fabrice Million <fabrice@funkadelik.com>
2020-03-24 12:06:38 +1100

    Impr: if useEntityText is null don't render an empty div

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,8 +1,7 @@
 import { PageIntroFullWidth } from '../../styles/MainContentStyles';
 import useEntityText from '../../utils/useEntityText';
 
-export default () => {
-  const Text = useEntityText('Description');
-
-  return <PageIntroFullWidth dangerouslySetInnerHTML={{ __html: Text }} />;
-};
+export default () =>
+  useEntityText('Description') && (
+    <PageIntroFullWidth dangerouslySetInnerHTML={{ __html: useEntityText('Description') }} />
+  );
--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -1,8 +1,4 @@
 import { Headline as StyledHeadline } from '../../styles/MainContentStyles';
 import useEntityText from '../../utils/useEntityText';
 
-export default () => {
-  const Text = useEntityText('Headline');
-
-  return <StyledHeadline>{Text}</StyledHeadline>;
-};
+export default () => useEntityText('Headline') && <StyledHeadline>{useEntityText('Headline')}</StyledHeadline>;
