2e27cfecfab786663f0e6831dd483a0d1344be53
Fabrice Million <fabrice@funkadelik.com>
2020-04-16 16:14:21 +1000

    feat: added pie chart to wporbo

--- src/layouts/workers-place-of-residence-occupation/page/index.tsx
+++ src/layouts/workers-place-of-residence-occupation/page/index.tsx
@@ -3,28 +3,27 @@ import dynamic from 'next/dynamic';
 import { useContext, useState } from 'react';
 import ControlPanel from '../../../components/ControlPanel/ControlPanel';
 import { MapLoader } from '../../../components/Map/MapLoader';
-import { MapWrapper, PageIntro, SourceBubble } from '../../../styles/MainContentStyles';
+import { MapWrapper, PageIntro, SourceBubble, ItemWrapper } from '../../../styles/MainContentStyles';
 import { PageContext, ClientContext } from '../../../utils/context';
-import { LinkBuilder } from '../../../components/ui/links';
+import { LinkBuilder, IdLink } from '../../../components/ui/links';
 import useEntityText from '../../../utils/useEntityText';
+import EntityChart from '../../../components/chart/EntityChart';
+import { formatPercent, idlogo } from '../../../utils';
 
 const LeafletMap = dynamic(() => import('../../../components/Map'), { ssr: false });
 // #endregion
 
 // #region template page
 const WorkersPlaceOfResidenceOccupationPage = () => {
-  console.log('WorkersPlaceOfResidenceOccupationPage');
   const [mapLoaded, setMapLoaded] = useState(false);
   const { clientAlias, LongName } = useContext(ClientContext);
-  const {
-    contentData,
-    entityData: { currentGenderName, currentAreaName, currentBenchmarkName, currentIndustryName, prefixedAreaName },
-  } = useContext(PageContext);
 
   const {
     contentData: { mapData },
+    entityData: { currentOccupationName, prefixedAreaName },
   } = useContext(PageContext);
 
+  console.log('mapData: ', mapData);
   const onMapLoaded = () => setMapLoaded(true);
 
   return (
@@ -70,9 +69,9 @@ const WorkersPlaceOfResidenceOccupationPage = () => {
         <MapLoader loaded={mapLoaded} />
         <LeafletMap mapData={mapData} onMapLoaded={onMapLoaded} />
       </MapWrapper>
-      {/* <ItemWrapper>
+      <ItemWrapper>
         <EntityChart data={chartBuilder()} />
-      </ItemWrapper> */}
+      </ItemWrapper>
       <p>
         NOTE: The land use shown in the map is derived from ABS Mesh Block categories. Mesh Blocks broadly identify land
         use and are not designed to provide definitive land use. It is purely an indicator of the main planned land use
@@ -82,4 +81,109 @@ const WorkersPlaceOfResidenceOccupationPage = () => {
   );
 };
 export default WorkersPlaceOfResidenceOccupationPage;
+// #endregion
+
+// #region sources
+const TableSource = () => (
+  <p>
+    Source: Australian Bureau of Statistics, 2011 and 2016. Compiled and presented by <IdLink />
+  </p>
+);
+
+// #endregion
+
+// #region chart builders
+const chartBuilder = () => {
+  const { clientAlias, LongName } = useContext(ClientContext);
+  const {
+    contentData: { mapData },
+    entityData: { currentOccupationName, prefixedAreaName },
+  } = useContext(PageContext);
+
+  const rawData = mapData.layers.find(l => l.id === 7).shapes.map(({ shapeOptions }) => shapeOptions);
+  const monash = rawData.filter(area => +area.GeoID === 24970)[0];
+  const monashTotal = monash.RealPercentage;
+  console.log('monashTotal: ', monashTotal);
+  const rest = rawData.filter(area => +area.GeoID != 24970);
+  const restTotal = rest.reduce((acc, cur) => {
+    return acc + cur.RealPercentage;
+  }, 0);
+  console.log('rest: ', rest);
+  // { name: LabelName, y: PerYear1, className: LabelName.match(/^[^\/ ]+/i) };
+  const serie = [
+    { name: 'Live and work in the area', y: monashTotal },
+    { name: 'Work in the area but live outside', y: restTotal },
+  ];
+  console.log('serie: ', serie);
+
+  const chartType = 'pie';
+  const chartTitle = `Residential location of local workers, 2016`;
+  const rawDataSource =
+    'Source: Australian Bureau of Statistics, Census of Population and Housing, 2016 Compiled and presented in economy.id by .id, the population experts.';
+  const chartContainerID = 'chart1';
+  const chartTemplate = 'Standard';
+
+  return {
+    rawDataSource,
+    dataSource: <TableSource />,
+    chartContainerID,
+    logoUrl: idlogo,
+    chartTemplate,
+    highchartOptions: {
+      chart: {
+        type: chartType,
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+      },
+      title: {
+        text: chartTitle,
+      },
+      subtitle: {
+        text: `${LongName} - ${currentOccupationName}`,
+      },
+      tooltip: {
+        pointFormatter: function() {
+          return `<span class="${this.className}">\u25CF</span> ${formatPercent(this.y)}%`;
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+          },
+          showInLegend: true,
+        },
+      },
+      series: [
+        {
+          data: serie,
+          colorByPoint: true,
+        },
+      ],
+      xAxis: {
+        type: 'category',
+        title: {
+          text: '',
+        },
+      },
+      legend: { enabled: false },
+      yAxis: [
+        {
+          title: {
+            text: '',
+          },
+          labels: {
+            staggerLines: 0,
+            formatter: function() {
+              return `${this.value}%`;
+            },
+          },
+        },
+      ],
+    },
+  };
+};
 // #endregion
