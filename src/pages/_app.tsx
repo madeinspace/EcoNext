76e9d6682895a7d0212c0d9b6fdacaa1c5de58c4
Fabrice Million <fabrice@funkadelik.com>
2020-05-06 09:11:06 +1000

    changed _app as per zeit example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,23 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable react/prop-types */
 import App from 'next/app';
-import React, { useEffect } from 'react';
 import '../styles/global.scss';
+import Router from 'next/router';
 import { initGA, logPageView } from '../utils/googleAnalytics';
 
-const MyComponent = props => {
-  const {
-    router: { query },
-    pageProps: { page },
-  } = props;
-  // If handle does not exist is must be a client landing page
-  const { handle } = page || { handle: query.clientAlias };
-  useEffect(() => {
-    initGA().then(() => {
-      logPageView({ clientAlias: query.clientAlias, page: handle });
-    });
-  });
-  return <>{props.children}</>;
-};
-
 export default class Economy extends App {
+  componentDidMount() {
+    const {
+      router: { query },
+      pageProps: { page },
+    } = this.props;
+    const { handle } = page || { handle: query.clientAlias };
+    initGA();
+    logPageView({ clientAlias: query.clientAlias, page: handle });
+    Router.events.on('routeChangeComplete', logPageView);
+  }
   render() {
     const { Component, pageProps } = this.props;
-    return (
-      <MyComponent {...this.props}>
-        <Component {...pageProps} />
-      </MyComponent>
-    );
+    return <Component {...pageProps} />;
   }
 }
--- src/utils/googleAnalytics/index.ts
+++ src/utils/googleAnalytics/index.ts
@@ -2,11 +2,23 @@
 
 import ReactGA from 'react-ga';
 
-export const initGA = async () => {
+export const initGA = () => {
   return ReactGA.initialize('UA-27808229-5');
 };
 
 export const logPageView = ({ clientAlias, page }) => {
   ReactGA.set({ page: page || 'economy.id.com.au', clientAlias: clientAlias || '.id' });
   ReactGA.pageview(page || 'economy.id.com.au');
+};
+
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action });
+  }
+};
+
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal });
+  }
 };
