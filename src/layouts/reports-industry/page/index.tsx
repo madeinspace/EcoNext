3bead4406dcf6ad7d5a85fb9c15d15843e72e08e
drfunkensztein <fabrice@id.com.au>
2020-07-01 13:57:57 +1000

    some refactor

--- src/layouts/reports-industry/page/index.tsx
+++ src/layouts/reports-industry/page/index.tsx
@@ -9,7 +9,7 @@ const ReportsIndustryPage = () => {
     contentData: { IndustryList1Digits, IndustryList2Digits },
   } = useContext(PageContext);
 
-  const getChildIndKey = ParentValue => {
+  const getChildIndValue = ParentValue => {
     const ParentLabel = IndustryList2Digits.filter(({ Value }) => Value === ParentValue)[0].Label;
     const childIndKey = IndustryList1Digits.filter(({ Label }) => Label === ParentLabel)[0];
     return childIndKey.Value;
@@ -17,7 +17,7 @@ const ReportsIndustryPage = () => {
 
   const indList = IndustryList2Digits.map(({ Label, ParentValue, Value }) => {
     const isParent = ParentValue == Value;
-    const value = isParent ? Value : getChildIndKey(ParentValue);
+    const value = isParent ? Value : getChildIndValue(ParentValue);
 
     const option = {
       label: Label,
@@ -26,7 +26,7 @@ const ReportsIndustryPage = () => {
       twoDigitKey: 'IndkeyNieir',
       oneDigitKey: 'Indkey',
       twoDigitValue: Value,
-      oneDigitValue: getChildIndKey(ParentValue),
+      oneDigitValue: getChildIndValue(ParentValue),
     };
     return option;
   });
