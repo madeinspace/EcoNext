67a8fa4e7bed0964db56c7a61222a0de385fdb51
drfunkensztein <fabrice@id.com.au>
2020-09-14 13:48:51 +1000

    Fix: deleted unused code

--- src/utils/sql/index.ts
+++ src/utils/sql/index.ts
@@ -1,17 +1,10 @@
 import knex from 'knex';
 
-const path = `.env.${process.env.NODE_ENV}`;
-require('dotenv').config({ path });
-
-const getScopedEnvVar = (scope, db_env_var) => {
-  return process.env[`${scope}_${db_env_var}`] || process.env[`DEFAULT_${db_env_var}`];
-};
-
 const client = 'mssql';
-const host = getScopedEnvVar('CLIENT', 'DATABASE_HOST');
-const user = getScopedEnvVar('CLIENT', 'DATABASE_USER');
-const password = getScopedEnvVar('CLIENT', 'DATABASE_PASS');
-const database = getScopedEnvVar('CLIENT', 'DATABASE_NAME');
+const host = `${process.env.DEFAULT_DATABASE_HOST}`;
+const user = `${process.env.DEFAULT_DATABASE_USER}`;
+const password = `${process.env.DEFAULT_DATABASE_PASS}`;
+const database = `${process.env.DEFAULT_DATABASE_NAME}`;
 const requestTimeout = 0;
 const options = { encrypt: false };
 
