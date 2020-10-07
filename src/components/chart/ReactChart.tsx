684ce622c18c8da43db064b801f557d20913139b
drfunkensztein <fabrice@id.com.au>
2020-10-07 11:03:16 +1100

    react chart added dumbbell

--- src/components/chart/ReactChart.tsx
+++ src/components/chart/ReactChart.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
+import HighChartsMore from 'highcharts/highcharts-more';
 import { Actions, ExportDropdown, ChartContainer } from '../Actions';
 import { Loader } from '../ui/loader';
 import { ChartDefault } from './configs/Chart.config';
@@ -9,6 +10,7 @@ import { ChartFooter } from './ChartFooter';
 import exporting from 'highcharts/modules/exporting';
 import offline from 'highcharts/modules/offline-exporting';
 import drilldown from 'highcharts/modules/drilldown';
+import dumbbell from 'highcharts/modules/dumbbell';
 import data from 'highcharts/modules/export-data';
 
 const ReactChart = props => {
@@ -30,10 +32,12 @@ const ReactChart = props => {
   });
 
   if (typeof Highcharts === 'object') {
+    HighChartsMore(Highcharts);
     exporting(Highcharts);
     offline(Highcharts);
     data(Highcharts);
     drilldown(Highcharts);
+    dumbbell(Highcharts);
   }
 
   const handleExport = item => {
